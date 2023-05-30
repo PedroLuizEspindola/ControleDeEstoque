a2445c83ffe358d783ade23fbc23b7344740a186
PedroLuizEspindola <130167969+PedroLuizEspindola@users.noreply.github.com>
2023-05-30 12:11:27 -0300

    add files via upload

---
+++ view/EstoqueView.java
@@ -0,0 +1,17 @@
+package view;
+
+import java.util.List;
+
+import model.Produto; 
+public class EstoqueView {
+    public void exibirProdutos(List<Produto> produtos) {
+        System.out.println("Estoque:");
+        for (Produto produto : produtos) {
+            System.out.println("Nome: " + produto.getNome());
+            System.out.println("Quantidade: " + produto.getQuantidade());
+            System.out.println("Preço: " + produto.getPreco());
+            System.out.println("--------------------");
+        }
+    }
+}
+
---
+++ view/PrincipalEstoque.java
@@ -0,0 +1,12 @@
+package view;
+
+
+import controller.EstoqueController;
+
+public class PrincipalEstoque {
+	public static void main(String[] args) {
+        EstoqueController controller = new EstoqueController();
+        
+	}
+}
+
